996bf644bacf774f73604de07bc4260fb8dacf27
leejungwu <ljw538@naver.com>
2021-08-25 06:09:11 +0900

    add loadPostsroute likers

--- back/routes/posts.ts
+++ back/routes/posts.ts
@@ -6,7 +6,7 @@ const Comment = db.Comment;
 
 const router = express.Router();
 
-router.get('/', async (req:Request, res:Response, next) => {  // POST /post/comment
+router.get('/', async (req:Request, res:Response, next) => {  // GET /posts
     try {
         const posts = await Post.findAll({  
             order: [['createdAt', 'DESC']],
@@ -15,8 +15,12 @@ router.get('/', async (req:Request, res:Response, next) => {  // POST /post/comm
             },
             include: [{
                 model: Comment,
+                attributes: ['id','nickname'],
+            },{
+                model: User, // 좋아요 누른 사람
+                as: 'Likers',
                 attributes: ['id'],
-            }] 
+              }] 
         });
         res.status(200).json(posts);
     } catch (error) {
