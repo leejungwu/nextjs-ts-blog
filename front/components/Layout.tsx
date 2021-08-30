21c54df55da7c60534fcd7c2a1d10be8c4ca955f
leejungwu <ljw538@naver.com>
2021-08-30 16:49:53 +0900

    fix: layout import

--- front/components/Layout.tsx
+++ front/components/Layout.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState, useEffect } from 'react'
 import Link from 'next/link'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
