23fee0e7fe5ef003b7b58be446319a5a7efd2802
leejungwu <ljw538@naver.com>
2021-08-30 16:51:53 +0900

    fix: useEffect import

--- front/pages/category/nodejs.tsx
+++ front/pages/category/nodejs.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Head from 'next/head';
 import styled from 'styled-components';
 import dayjs from 'dayjs';
--- front/pages/category/react.tsx
+++ front/pages/category/react.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react'
 import Head from 'next/head';
 import styled from 'styled-components';
 import dayjs from 'dayjs';
